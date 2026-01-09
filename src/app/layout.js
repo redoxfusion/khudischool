8611511e93f794d71cdd7de99ff74cd6364c327c
khubaib-ctrl <khubaib@homeeasy.com>
2026-01-09 12:53:46 +0500

    fix 3

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,42 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "KHUDI INSTITUTE",
-  description: "A premier institute for learning and development",
+  title: {
+    default: "Khudi Institute | Alternative School & Homeschool Resource Center",
+    template: "%s | Khudi Institute",
+  },
+  description: "Khudi Institute is an alternative school and resource center for homeschoolers, focused on the holistic development of each learner through project-based learning rooted in Islamic values.",
+  keywords: ["Khudi Institute", "alternative school", "homeschool", "Islamic education", "project-based learning", "holistic development"],
+  authors: [{ name: "Khudi Institute" }],
+  creator: "Khudi Institute",
+  publisher: "Khudi Institute",
+  metadataBase: new URL("https://www.khudi.institute"),
+  openGraph: {
+    title: "Khudi Institute | Alternative School & Homeschool Resource Center",
+    description: "Khudi Institute is an alternative school and resource center for homeschoolers, focused on the holistic development of each learner through project-based learning rooted in Islamic values.",
+    url: "https://www.khudi.institute",
+    siteName: "Khudi Institute",
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Khudi Institute | Alternative School & Homeschool Resource Center",
+    description: "Khudi Institute is an alternative school and resource center for homeschoolers, focused on the holistic development of each learner through project-based learning rooted in Islamic values.",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
   icons: {
-    icon: "/logo.svg", // path to your uploaded image
+    icon: "/logo.svg",
   },
 };
 
